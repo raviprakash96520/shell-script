469ef9b5f4bfa42ec5502c8eddfd995d86f689d5
raviprakash96520 <raviprakash96520@gmail.com>
2025-06-08 15:56:37 +0530

    added

---
+++ 05-user-input.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Please enter username:: "
+
+read USERNAME
+
+echo "entere username is :: $USERNAME"
+
+echo "enter your Password :: "
+
+read PASSWORD
+
+echo "entered password :: $PASSWORD"
+
